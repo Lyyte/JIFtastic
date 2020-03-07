de1b89ec764a4cd11ff32e205b4b1f1426e55bdf
Lyyte <lyytefaridi@gmail.com>
2020-03-07 17:40:38 -0600

    can make new buttons, but is including full array

--- assets/game.js
+++ assets/game.js
@@ -1,19 +1,32 @@
-$(document).ready(function() {
-var topics = ["tennis", "soccer", "baseball", "basketball", "football"];
-
-for (i=0; i<topics.length; i++ ) {
-    var button = $("<button>");
-    button.attr("class", "btn btn-primary");
-    button.attr("type", "submit");
-    button.attr("value", "Submit");
-    button.text(topics[i]);
-    $("#sportbuttons").append(button);
+$(document).ready(function () {
+    var topics = ["tennis", "soccer", "baseball", "basketball", "football"];
 
 
+    function renderButtons () {
+    for (i = 0; i < topics.length; i++) {
+        var button = $("<button>");
+        button.attr("class", "btn btn-primary");
+        button.attr("type", "submit");
+        button.attr("value", "Submit");
+        button.text(topics[i]);
+        $("#sportbuttons").append(button);
+    }
+    };
 
-   // <input class="btn btn-primary" type="submit" value="Submit"></input>
+    renderButtons();
+
+    $("#newbtn").on("click", function(event) {
+        event.preventDefault();
+        var newsport = $("#newbutton").val().trim();
+        topics.push(newsport);
+        console.log(topics[i])
+        renderButtons();
+
+    });
 
 
 
-}
+
+
+
 }); 
