3cd2cdfff8c6027edef377a5f26f8a837b1353c4
Lyyte <lyytefaridi@gmail.com>
2020-03-07 19:46:50 -0600

    added '&' in api key and set to empty div on button press

--- assets/game.js
+++ assets/game.js
@@ -8,12 +8,35 @@ $(document).ready(function () {
     for (i = 0; i < topics.length; i++) {
         var button = $("<button>");
         button.attr("class", "btn btn-primary sportB");
-        button.attr("type", "submit");
-        button.attr("value", "Submit");
         button.attr("data-sport", topics[i]);
         button.text(topics[i]);
         $("#sportbuttons").append(button);
     }
+
+    $(".sportB").on("click", function() {
+        $("#gifs").empty();
+        var sport = $(this).attr("data-sport");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          sport + "&api_key=lPHmsdHCzhKDLregWToTRZAE6EIoPgFR&limit=5";
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+              if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                var gifDiv = $("<div>");
+                var sportImage = $("<img>");
+                sportImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.append(sportImage);
+                $("#gifs").prepend(gifDiv);
+              }
+            }
+          });
+        });
+
     };
 
     $("#newbtn").on("click", function(event) {
@@ -25,31 +48,7 @@ $(document).ready(function () {
 
     });
 
-    $("sportB").on("click", function() {
-        var sport = $(this).attr("data-sport");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-          sport + "&api_key=lPHmsdHCzhKDLregWToTRZAE6EIoPgFRlimit=5";
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-          .then(function(response) {
-            var results = response.data;
-            for (var i = 0; i < results.length; i++) {
-              if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                var gifDiv = $("<div>");
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
-                var sportImage = $("<img>");
-                sportImage.attr("src", results[i].images.fixed_height.url);
-                gifDiv.append(p);
-                gifDiv.append(sportImage);
-                $("#gifs").prepend(gifDiv);
-              }
-            }
-          });
-        });
-
+   
 
 
 
