5266a05935526c8fc51e74b1e5d476f0ca92564b
Lyyte <lyytefaridi@gmail.com>
2020-03-07 18:45:11 -0600

    added ajax function, create new class for ajax to ref, not working

--- assets/game.js
+++ assets/game.js
@@ -1,32 +1,57 @@
 $(document).ready(function () {
     var topics = ["tennis", "soccer", "baseball", "basketball", "football"];
+    renderButtons();
 
 
     function renderButtons () {
+    $("#sportbuttons").empty();
     for (i = 0; i < topics.length; i++) {
         var button = $("<button>");
-        button.attr("class", "btn btn-primary");
+        button.attr("class", "btn btn-primary sportB");
         button.attr("type", "submit");
         button.attr("value", "Submit");
+        button.attr("data-sport", topics[i]);
         button.text(topics[i]);
         $("#sportbuttons").append(button);
     }
     };
 
-    renderButtons();
-
     $("#newbtn").on("click", function(event) {
         event.preventDefault();
         var newsport = $("#newbutton").val().trim();
         topics.push(newsport);
-        console.log(topics[i])
+        console.log(topics[i]);
         renderButtons();
 
     });
 
+    $("sportB").on("click", function() {
+        var sport = $(this).attr("data-sport");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          sport + "&api_key=lPHmsdHCzhKDLregWToTRZAE6EIoPgFRlimit=5";
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+              if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                var gifDiv = $("<div>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var sportImage = $("<img>");
+                sportImage.attr("src", results[i].images.fixed_height.url);
+                gifDiv.append(p);
+                gifDiv.append(sportImage);
+                $("#gifs").prepend(gifDiv);
+              }
+            }
+          });
+        });
 
 
 
 
 
-}); 
+        });
